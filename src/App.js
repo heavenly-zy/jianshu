25cfb59346594f1404551c53824bba7fd7c12d71
zy <1457715339@qq.com>
2020-03-29 12:20:39 +0800

    使用 react-router-dom 路由

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import Header from './common/header/index.js';
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <Header />
+        <BrowserRouter>
+          <Route path='/' exact render={() => <div>home</div>}></Route>
+          <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+        </BrowserRouter>
       </Provider>
     )
   }
