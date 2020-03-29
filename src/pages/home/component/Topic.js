ea258facac90e5d9716ab7bf82a5aaf318ab5736
zy <1457715339@qq.com>
2020-03-29 16:21:30 +0800

    完成 Topic 组件布局

--- src/pages/home/component/Topic.js
+++ src/pages/home/component/Topic.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { TopicWrapper, TopicItem } from '../style';
 
 class Topic extends Component {
   render() {
     return (
-      <div>Topic</div>
+      <TopicWrapper>
+        <TopicItem>
+          <div className="topic-pic"></div>
+          <span>社会热点</span>
+        </TopicItem>
+        <TopicItem>
+          <div className="topic-pic"></div>
+          <span>社会热点</span>
+        </TopicItem>
+        <TopicItem>
+          <div className="topic-pic"></div>
+          <span>社会热点</span>
+        </TopicItem>
+        <TopicItem>
+          <div className="topic-pic"></div>
+          <span>社会热点</span>
+        </TopicItem>
+        <TopicItem>
+          <div className="topic-pic"></div>
+          <span>社会热点</span>
+        </TopicItem>
+        <TopicItem>
+          <div className="topic-pic"></div>
+          <span>社会热点</span>
+        </TopicItem>
+        <TopicItem>
+          <div className="topic-pic"></div>
+          <span>社会热点</span>
+        </TopicItem>
+        <TopicItem>
+          <div className="topic-pic"></div>
+          <span>社会热点</span>
+        </TopicItem>
+      </TopicWrapper>
     )
   }
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -4,11 +4,9 @@ export const HomeWrapper = styled.div`
   overflow: hidden;
   width: 960px;
   margin: 0 auto;
-  border: 1px solid red;
 `
 
 export const HomeLeft = styled.div`
-  border: 1px solid green;
   float: left;
   margin-left: 15px;
   padding-top: 30px;
@@ -23,7 +21,35 @@ export const HomeLeft = styled.div`
 `
 
 export const HomeRight = styled.div`
-border: 1px solid yellow;
   width: 240px;
   float: right;
+`
+
+export const TopicWrapper = styled.div`
+  overflow: hidden;
+  padding: 20px 0 10px 0;
+  margin-left: -18px;
+`
+
+export const TopicItem = styled.div`
+  float: left;
+  height: 32px;
+  line-height: 32px;
+  margin-left: 18px;
+  margin-bottom: 18px;
+  padding-right: 10px;
+  background: #f7f7f7;
+  font-size: 14px;
+  color: #000;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+  .topic-pic {
+    background-image: url(${require("../../statics/topic.png")});
+    background-position: center center;
+    background-size: contain;
+    width: 32px;
+    height: 100%;
+    margin-right: 10px;
+    float: left;
+  }
 `
