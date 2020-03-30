829d63b9bcf31d66005178c4eefa299433f8bd2f
zy <1457715339@qq.com>
2020-03-30 21:01:01 +0800

    优化 home/reducer 代码

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -9,19 +9,27 @@ const defaultState = fromJS({
   showScroll: false
 });
 
+const changeHomeData = (state, action) => {
+  return state.merge({
+    topicList: fromJS(action.topicList),
+    articleList: fromJS(action.articleList),
+    recommendList: fromJS(action.recommendList)
+  });
+}
+
+const addArticleList = (state, action) => {
+  return state.merge({
+    articleList: state.get('articleList').concat(action.list),
+    articlePage: fromJS(action.nextPage)
+  });
+}
+
 export default (state = defaultState, action) => {
   switch (action.type) {
     case actionType.CHANGE_HOME_DATA:
-      return state.merge({
-        topicList: fromJS(action.topicList),
-        articleList: fromJS(action.articleList),
-        recommendList: fromJS(action.recommendList)
-      });
+      return changeHomeData(state, action);
     case actionType.ADD_ARTICLE_LIST:
-      return state.merge({
-        articleList: state.get('articleList').concat(action.list),
-        articlePage: fromJS(action.nextPage)
-      });
+      return addArticleList(state, action);
     case actionType.TOGGLE_SCROLL_TOP:
       return state.set('showScroll', action.showScroll)
     default:
