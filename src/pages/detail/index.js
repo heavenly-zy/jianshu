3d48617511c727659910a7ed3de41cfe05468b65
zy <1457715339@qq.com>
2020-04-01 10:52:08 +0800

    完成详情页布局

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,23 @@
 import React, { PureComponent } from 'react';
+import {
+  DetailWrapper,
+  Header,
+  Content
+} from './style'
 
 class Detail extends PureComponent {
   render() {
     return (
-      <div>Detail</div>
+      <DetailWrapper>
+        <Header>ES6 新增数据类型</Header>
+        <Content>
+          <img src={require('../../statics/banner.png')} alt="banner" />
+          <p><b>ES6 引入了一种新的原始数据类型Symbol，表示独一无二的值。</b><br/>它是 JavaScript 语言的第七种数据类型，前六种是：undefined、null、布尔值（Boolean）、字符串（String）、数值（Number）、对象（Object）。</p>
+          <p><b>ES6 引入了一种新的原始数据类型Symbol，表示独一无二的值。</b><br/>它是 JavaScript 语言的第七种数据类型，前六种是：undefined、null、布尔值（Boolean）、字符串（String）、数值（Number）、对象（Object）。</p>
+          <p><b>ES6 引入了一种新的原始数据类型Symbol，表示独一无二的值。</b><br/>它是 JavaScript 语言的第七种数据类型，前六种是：undefined、null、布尔值（Boolean）、字符串（String）、数值（Number）、对象（Object）。</p>
+          <p><b>ES6 引入了一种新的原始数据类型Symbol，表示独一无二的值。</b><br/>它是 JavaScript 语言的第七种数据类型，前六种是：undefined、null、布尔值（Boolean）、字符串（String）、数值（Number）、对象（Object）。</p>
+        </Content>
+      </DetailWrapper>
     )
   }
 }
