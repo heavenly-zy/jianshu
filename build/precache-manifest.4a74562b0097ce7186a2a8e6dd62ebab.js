13a89c12353ef1241fccddee20fca63f87c3144c
zy <1457715339@qq.com>
2020-04-01 22:06:12 +0800

    build

---
+++ build/precache-manifest.4a74562b0097ce7186a2a8e6dd62ebab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0699811c1e4b697cf39fe0185316ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9beab01cc10ac8ce5d55",
+    "url": "./static/js/2.37485d2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.37485d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5149f6f710ff3a1d562",
+    "url": "./static/js/main.4b13aa44.chunk.js"
+  },
+  {
+    "revision": "27bfa36776d47ad63d70",
+    "url": "./static/js/runtime-main.669a0d40.js"
+  },
+  {
+    "revision": "eee1b1975bc9ecf0f44dc944383efa62",
+    "url": "./static/media/banner.eee1b197.png"
+  },
+  {
+    "revision": "aa05af19c50431f4f7a863de94840b34",
+    "url": "./static/media/topic1.aa05af19.png"
+  },
+  {
+    "revision": "f060de33c00f304f4bb57e58f9166d74",
+    "url": "./static/media/topic2.f060de33.png"
+  }
+]);
